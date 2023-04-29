bc932077d87f5890d3a4a3ba9174a89d2b43c34c
sh123 <sh123@users.noreply.github.com>
2023-04-29 11:30:39 +0300

    Disable gpio usage for fan and oscil and fix rpi4 64 bit detection

--- remsdr/remsdr_modules/CPU.js
+++ remsdr/remsdr_modules/CPU.js
@@ -8,13 +8,13 @@ const cp = require('child_process');
 const Gpio = require('onoff').Gpio; //to interact with the GPIO
 const fs = require('fs');
 var info_cpu = cp.execSync("lscpu").toString();
-if (info_cpu.indexOf("armv7l") > 0) {
+if (info_cpu.indexOf("aarch64") > 0) {
     Model = "Raspberry PI4";
     CPUshort = "RPI4";
     GPIO_Fan = "4"; // Pin 7 on Raspberry
     GPIO_Oscil = "7";
 }
-if (info_cpu.indexOf("aarch64") > 0) {
+/*if (info_cpu.indexOf("aarch64") > 0) {
     Model = "Orange PI Zero One Plus";
     CPUshort = "opi1p";
     if (info_cpu.indexOf("1512.0") > 0) {
@@ -22,18 +22,18 @@ if (info_cpu.indexOf("aarch64") > 0) {
         CPUshort = "opiz2";
         GPIO_Oscil = "74";
     }
-}
-var Fan_Pin = new Gpio(GPIO_Fan, 'out');
+}*/
+//var Fan_Pin = new Gpio(GPIO_Fan, 'out');
 function ReadTemperature() {
     var adr_Temp = '/sys/class/thermal/thermal_zone0/temp';
     fs.readFile(adr_Temp, 'utf8', function (err, data) { //Asynchrone read
         var T = Math.floor(parseInt(data) / 1000);
         Temperature = T + '°C';
         if (T >= 65) {
-            Fan_Pin.write(1);
+            //Fan_Pin.write(1);
             Fan_State = "On";
         } else {
-            Fan_Pin.write(0);
+            //Fan_Pin.write(0);
             Fan_State = "Off";
         }
     });
@@ -41,12 +41,12 @@ function ReadTemperature() {
 //
 //Oscillator when TX
 //*******************
-var Oscil_Pin = new Gpio(GPIO_Oscil, 'out');
+//var Oscil_Pin = new Gpio(GPIO_Oscil, 'out');
 var Oscil_state = 0;
-Oscil_Pin.write(Oscil_state);
+//Oscil_Pin.write(Oscil_state);
 function ToggleOscil() {
     Oscil_state = (Oscil_state + 1) % 2;
-    Oscil_Pin.write(Oscil_state);
+    //Oscil_Pin.write(Oscil_state);
 }
 var Oscil_interval;
 function ToggleOscil_10s() {
